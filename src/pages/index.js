ca394dd38d41e33936aa9491f3277ec2f80f7649
mrnithish <g.nithish100@gmail.com>
2024-06-06 15:30:36 +0530

    resume update

--- src/pages/index.js
+++ src/pages/index.js
@@ -50,7 +50,7 @@ export default function Home() {
               </p>
               <div className="flex items-center self-start mt-2 lg:self-center">
                 <Link
-                  href="https://drive.google.com/file/d/1wH_XlsI1S8Rch-XwXBdRKH1n7MDG5Wgj/view?usp=drive_link"
+                  href="https://drive.google.com/file/d/1ZvmNNr6i78oNWu5xH8xdgx72R-mqCZ0V/view?usp=drive_link"
                   target={"_blank"}
                   className="flex items-center bg-dark text-light p-2.5 px-6 
                rounded-lg text-lg font-semibold hover:bg-light hover:text-dark 
